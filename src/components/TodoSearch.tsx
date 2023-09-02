59e2e0f62a1d4c72bc2993f49fbc3b7a43366b62
akhaniskak <alex12hitman@gmail.com>
2023-09-02 19:17:56 +0600

    validate email

--- src/components/TodoSearch.tsx
+++ src/components/TodoSearch.tsx
@@ -9,5 +9,4 @@ export default function SearchTodo({searchTodo}: { searchTodo: any }) {
             placeholder="Todo name"
         />
     )
-
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,49 +1,77 @@
-import {Container, Typography, Input, Button, Link} from "@mui/material";
+import {Container, Typography, Button, Link, TextField} from "@mui/material";
 import {useState} from "react";
 import {useRouter} from "next/router";
 import {loginUser} from "@/api/auth";
 
-
 export default function Login() {
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
     const router = useRouter();
+
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        if (!email || !password){
-            alert('Please set email or password')
+        e.preventDefault();
+
+        // Validate email before submitting
+        if (!email || !password) {
+            alert("Please enter a valid email");
+            return;
+        }
+        if (!validateEmail(email)) return
+        const response = await loginUser({
+            email,
+            password,
+        });
+
+        if (response.statusCode === 401) {
+            alert(response.message);
             return;
         }
 
-        e.preventDefault()
-        const response = await loginUser({
-            email,
-            password
-        });
-        if(response.statusCode===401) {
-            alert(response.message);
-            return
-        }
-        localStorage.setItem('token',response.accessToken)
-        await router.push('/todo')
-    }
+        localStorage.setItem("token", response.accessToken);
+        await router.push("/todo");
+    };
+
+    const validateEmail = (value: string) => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        return emailRegex.test(value);
+    };
+
     return (
-        <Container className='container-styles'>
-            <Typography variant="h4" textAlign='center'>Login</Typography>
-            <Link variant="h6" textAlign='center' href='/register'>Or Register</Link>
+        <Container className="container-styles">
+            <Typography variant="h4" textAlign="center">
+                Login
+            </Typography>
+            <Link variant="h6" textAlign="center" href="/register">
+                Or Register
+            </Link>
 
             <form onSubmit={handleSubmit}>
-                <label>
-                    <Typography>Email</Typography>
-                    <Input onChange={e => setEmail(e.target.value)}></Input>
-                </label>
-                <label>
-                    <Typography>Password</Typography>
-                    <Input onChange={e => setPassword(e.target.value)}></Input>
-                </label>
                 <div>
-                    <Button type="submit" variant='contained'>Login</Button>
+                    <Typography>Email</Typography>
+                    <TextField
+                        error={!validateEmail(email)}
+                        helperText={!validateEmail(email) ? "Invalid email format" : ""}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        fullWidth
+                        required
+                    />
+                </div>
+                <div>
+                    <Typography>Password</Typography>
+                    <TextField
+                        onChange={(e) => setPassword(e.target.value)}
+                        type="password"
+                        fullWidth
+                        required
+                    />
+                </div>
+                <div>
+                    <Button type="submit" variant="contained">
+                        Login
+                    </Button>
                 </div>
             </form>
         </Container>
-    )
+    );
 }
